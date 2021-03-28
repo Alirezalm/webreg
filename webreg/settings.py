d03d7a5bca2f1962b53539fb9ec3ec13bc112d83
alireza <alireza.lm69@gmail.com>
2021-03-28 19:37:13 -0300

    release 1.0.0

--- webreg/settings.py
+++ webreg/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2qx&9$u%xrtq$@+v+9b1^(orx-ij9+wbzps(#zf0b&96wx^668'
-
+# SECRET_KEY = '2qx&9$u%xrtq$@+v+9b1^(orx-ij9+wbzps(#zf0b&96wx^668'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '2qx&9$u%xrtq$@+v+9b1^(orx-ij9+wbzps(#zf0b&96wx^668')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.172']
 
